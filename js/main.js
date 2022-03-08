571ebfea09754936eec0a971c57851f83b08d8e8
cif95 <vivianacifarelli9@gmail.com>
2022-03-08 15:33:34 +0100

    implemented script

--- js/main.js
+++ js/main.js
@@ -2,3 +2,36 @@
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// creating 5 random integers in 5 different output dom Elements
+for ( let i = 1; i <= 5; i++){
+	document.getElementById(`num-${i}`).innerHTML = randomInt(1, 150);
+}
+let resultOutput = document.querySelector('#result-output');
+let userCorrectAnswer = 0 ;
+
+setTimeout(function() {
+	document.querySelector('#numbers-group').classList.add('d-none');
+	for ( let i = 1; i <= 5; i++){
+		let userAnswer = parseInt(prompt(`Insert number ${i} `));
+		console.log(userAnswer);
+		if (document.getElementById(`num-${i}`).innerHTML == userAnswer){
+			userCorrectAnswer++;
+			resultOutput.innerHTML += ` User guessed number ${i} `
+		}
+	}
+	resultOutput.innerHTML += `Totally, user guessed ${userCorrectAnswer} numbers`
+}, 3000);
+
+
+
+/**Function that creates a random integer between two values
+ * 
+ * @param {*} min minimum value of the random integer 
+ * @param {*} max maximum value of the random integer 
+ * @returns a random integer between a min and a max value
+ */
+function randomInt (min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
